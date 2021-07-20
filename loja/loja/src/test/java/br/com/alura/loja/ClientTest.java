aef0051fe4074f0ddacf6f2dab4762203c23ffd5
jhonatan <podearroztkd@gmail.com>
2021-07-20 17:06:16 -0300

    Testes end-to-end com JAX-RS e Grizzly
    Aula 02: JAX-RS e Jersey: Domine a criação de webservice REST

--- loja/loja/src/test/java/br/com/alura/loja/ClientTest.java
+++ loja/loja/src/test/java/br/com/alura/loja/ClientTest.java
@@ -4,27 +4,54 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
 
 public class ClientTest {
 	
+	
+	
+	private HttpServer server;
+
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+	}
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+
+	}
+		
+	
 	@Test
-	public void testaQueAConexaoComOServidorFunciona() {
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 		
 	}
 	
 	@Test
-    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+    public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
         String conteudo = target.path("/projetos").request().get(String.class);
-        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
-
-
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
     }
+
+
+    
 }
